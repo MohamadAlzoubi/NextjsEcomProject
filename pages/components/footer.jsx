afd5bbb85328220b8f01878e786e8ee87452a424
mohamadAlzoubi <mohamad.alzoubi.b@gmail.com>
2023-06-22 03:56:32 +0300

    last updates

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -2,38 +2,47 @@ import React , {useState} from 'react';
 
 const Footer = () => {
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <footer className={`footer`}>
-      
-      <div className="logo">
-        <img src="./logos/logodark.png" alt="" />
+      <div className="row main-wrapper">
+        <div className="col-lg-9">
+          <div className="row loggo-links-wrapper">
+            <div className="col-lg-3">
+              <div className="logo">
+                <img src="./logos/logodark.png" alt="" />
+              </div>
+            </div>
+              <div className="col-lg-9">
+                  <ul className={`footer-menu`}>
+                  <li className="footer_links">
+                     Home
+                  </li>
+                  <li className="footer_links">
+                    Community
+                  </li>
+                  <li className="footer_links">
+                    Blog
+                  </li>
+                  <li className="footer_links">
+                    Events
+                  </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3">
+        <div className='row social_accounts'>
+          <img src="./images/facebook.png" alt="" />
+          <img src="./images/instagram.png" alt="" />
+          <img src="./images/Linkedin.png" alt="" />
+          <img src="./images/Twitter.png" alt="" />
+        </div>
+        </div>
+     
+     
       </div>
-      <ul className={`footer-menu`}>
-      <li className="footer_links">
-          Home
-        </li>
-        <li className="footer_links">
-          Community
-        </li>
-        <li className="footer_links">
-          Blog
-        </li>
-        <li className="footer_links">
-          Events
-        </li>
-      </ul>
-      <div className='row social_accounts'>
-        <img src="./images/facebook.png" alt="" />
-        <img src="./images/instagram.png" alt="" />
-        <img src="./images/Linkedin.png" alt="" />
-        <img src="./images/Twitter.png" alt="" />
-      </div>
+    
     </footer>
   );
 };
